2c5aad99f454741cde3ce5280ad5ac2fe8c15584
Frank Navarro <franknavarro@users.noreply.github.com>
2017-04-13 15:13:46 -0700

    Final Touches

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
+$(window).load(function () {
+    //normally you'd wait for document.ready, but you'd likely to want to wait
+    //for images to load in case they reflow the page
+    $('body').delay(7000) //wait 7 seconds
+        .animate({
+            //animate jQuery's custom "scrollTop" style
+            //grab the value as the offset of #second from the top of the page
+            'scrollTop': $('#diff-times').offset().top
+        }, 750); //animate over 750ms, change this to however long you want it to animate for
+});
+
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 750);
     return false;
-});
-
-$(window).load(function () {
-    //normally you'd wait for document.ready, but you'd likely to want to wait
-    //for images to load in case they reflow the page
-    $('body').delay(10000) //wait 10 seconds
-        .animate({
-            //animate jQuery's custom "scrollTop" style
-            //grab the value as the offset of #second from the top of the page
-            'scrollTop': $('#diff-times').offset().top
-        }, 750); //animate over 300ms, change this to however long you want it to animate for
 });
