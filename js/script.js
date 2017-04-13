c2be5bb4939417ac8de262ffd9c3cf6565868307
Frank Navarro <franknavarro@users.noreply.github.com>
2017-04-12 22:27:43 -0700

    Edit Responsive Layout

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $('a').click(function(){
 $(window).load(function () {
     //normally you'd wait for document.ready, but you'd likely to want to wait
     //for images to load in case they reflow the page
-    $('body').delay(5000) //wait 5 seconds
+    $('body').delay(10000) //wait 10 seconds
         .animate({
             //animate jQuery's custom "scrollTop" style
             //grab the value as the offset of #second from the top of the page
