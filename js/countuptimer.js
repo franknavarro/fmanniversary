c9be254eb78f02c8bd4af541438f65a575b0c517
Frank Navarro <franknavarro@users.noreply.github.com>
2017-04-12 20:32:55 -0700

    Added Smooth Scrolling

--- js/countuptimer.js
+++ js/countuptimer.js
@@ -1,6 +1,7 @@
 window.onload=function() {
     upTime('april,17,2016,04:15:00'); // ****** Change this line!
 }
+
 function upTime(countTo) {
     now = new Date();
     countTo = new Date(countTo);
@@ -34,6 +35,34 @@ function upTime(countTo) {
     document.getElementById('total-hours').firstChild.nodeValue = total_hours;
     document.getElementById('total-mins').firstChild.nodeValue = total_mins;
     document.getElementById('total-secs').firstChild.nodeValue = total_secs;
+    
+    if (years == 1) {
+        document.getElementById('year-s').innerHTML = "";
+        document.getElementById('total-year-s').innerHTML = "";
+    } else {
+        document.getElementById('year-s').innerHTML = "s";
+        document.getElementById('total-year-s').innerHTML = "s";
+    }
+    if (days == 1) {
+        document.getElementById('day-s').innerHTML = "";
+    } else {
+        document.getElementById('day-s').innerHTML = "s";
+    }
+    if (hours == 1) {
+        document.getElementById('hour-s').innerHTML = "";
+    } else {
+        document.getElementById('hour-s').innerHTML = "s";
+    }
+    if (mins == 1) {
+        document.getElementById('min-s').innerHTML = "";
+    } else {
+        document.getElementById('min-s').innerHTML = "s";
+    }
+    if (secs == 1) {
+        document.getElementById('sec-s').innerHTML = "";
+    } else {
+        document.getElementById('sec-s').innerHTML = "s";
+    }
     
     
 
