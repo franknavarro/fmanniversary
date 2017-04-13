c116e24f748b632ba06eb3d903ea0c765aa13cd3
Frank Navarro <franknavarro@users.noreply.github.com>
2017-04-12 22:08:38 -0700

    Added Floating Hearts

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $('a').click(function(){
     }, 750);
     return false;
 });
+
+$(window).load(function () {
+    //normally you'd wait for document.ready, but you'd likely to want to wait
+    //for images to load in case they reflow the page
+    $('body').delay(5000) //wait 5 seconds
+        .animate({
+            //animate jQuery's custom "scrollTop" style
+            //grab the value as the offset of #second from the top of the page
+            'scrollTop': $('#diff-times').offset().top
+        }, 750); //animate over 300ms, change this to however long you want it to animate for
+});
